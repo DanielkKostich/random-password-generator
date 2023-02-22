490a4ff9a30fbbb74f7491272c19032c2ced36e2
DanielkKostich <117228151+DanielkKostich@users.noreply.github.com>
2023-02-22 15:25:03 -0600

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,33 @@
-// Assignment code here
+// Assignment code here 
+function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+var generateButton = document.createElement("button");
+     
+
+
+document.querySelector('#generate').addEventListener('click', promptMe);
+
+function promptMe() {
+    var number = prompt("Password length 8-128 characters");
+    var type = prompt("Character type");
+    
+}
+
+
+
+
+
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
 function writePassword() {
@@ -14,4 +39,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", prompt("hello"))
+generateBtn.addEventListener("click",writePassword) ;
