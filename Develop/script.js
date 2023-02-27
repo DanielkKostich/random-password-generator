a0087c165a32ba9a5ade66b876b59f3e1b59bff8
DanielkKostich <117228151+DanielkKostich@users.noreply.github.com>
2023-02-27 11:57:33 -0600

    got it workinggit add -A!

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,81 @@
 // Assignment code here 
 function generatePassword() {
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&+",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
+  var retVal = "";
+  var charset = "";
+
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numericCase = "0123456789"
+  var specialCase = "!@#$%&+"
+  var long = window.prompt("How long? 8-128")
+  if (long < 8 || long > 128 || isNaN(long) ){
+    window.alert("Invalid") 
+    return ""
+  }
+  
+ var useLower = window.confirm("use lowercase?")
+  if (useLower){
+    charset+= lowerCase
+  }
+
+  var useUpper = window.confirm("use Uppercase?")
+  if (useLower){
+    charset+= upperCase
+  }
+
+  var useNumeric = window.confirm("use Numbers?")
+  if (useLower){
+    charset+= numericCase
+  }
+
+  var useSpecial = window.confirm("use Special characters?")
+  if (useLower){
+    charset+= specialCase
+  }
+
+if (useLower + useUpper + useNumeric + useSpecial  <1){
+  window.alert("Please choose on type")
+}
+  
+  for (var i = 0; i < long; i++) {
+    retVal += charset.charAt(Math.floor(Math.random() * charset.length));
   }
   return retVal;
 }
 var generateButton = document.createElement("button");
-     
-
-
-document.querySelector('#generate').addEventListener('click', promptMe);
-
-
-function promptMe(){
-     	  
-      var num = prompt("password lenth");
-      
-  if (!isNaN(num)){  
-   
-    var char = prompt("Char type")
-  
-  if (char == null){
-      alert("invalid")
-      promptMe()
-  }
-  if (!isNaN(char) ){
-    alert("invalal")
-    promptMe()
-}
-}
-   
-  
-  else { alert("Invalid Input.") 
-      promptMe()
-    }
-
-   
-  
 
 
 
-      
-  }
 
 
-const value = document.querySelector("prompt")
+// function promptMe(){
+
+//       var num = prompt("password lenth");
+
+//   if (!isNaN(num)){  
+
+//     var char = prompt("Char type")
+
+//   if (char == null){
+//       alert("invalid")
+//       promptMe()
+//   }
+//   if (!isNaN(char) ){
+//     alert("invalal")
+//     promptMe()
+// }
+// }
+
+
+//   else { alert("Invalid Input.") 
+//       promptMe()
+//     }
+
+
+//   }
+
+
+
 
 
 
@@ -68,4 +96,4 @@ function writePassword() {
 
 // Add event listener to generate button
 //generateBtn.addEventListener("click", prompt("hello"))
-generateBtn.addEventListener("click",writePassword) ;
+generateBtn.addEventListener("click", writePassword);
